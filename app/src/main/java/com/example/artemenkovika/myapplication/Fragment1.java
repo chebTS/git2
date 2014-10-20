c9d51c848fff8ffc1cd5833ef4c4e5852cb0c600
Vika <vikaiisus2012@yandex.ua>
2014-10-20 21:32:02 +0300

    first commit

---
+++ app/src/main/java/com/example/artemenkovika/myapplication/Fragment1.java
@@ -0,0 +1,45 @@
+package com.example.artemenkovika.myapplication;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Fragment1 extends Fragment {
+    TextView txt;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_fragment1, null);
+        txt = (TextView) v.findViewById(R.id.textView);
+        Button btn1 = (Button) v.findViewById(R.id.button1);
+        Button btn2 = (Button) v.findViewById(R.id.button2);
+        Button btn3 = (Button) v.findViewById(R.id.button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Нажата кнопка \"Кнопка 1\"");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Нажата кнопка \"Кнопка 2\"");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Нажата кнопка \"Кнопка 3\"");
+            }
+        });
+
+        return v;
+    }
+
+
+}
